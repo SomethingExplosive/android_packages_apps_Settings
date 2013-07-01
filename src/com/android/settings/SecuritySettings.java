748f39eb856c955b5b016069b3091b2541a17063
Lars Greiss <kufikugel@googlemail.com>
2013-07-02 01:46:10 +0200

    Settings: Introduce Privacy Guard Manager
    NOTE: This is a squash of 3 commits, including fixes for the Privacy Guard Manager.
    
    NOTE: This includes all existing CM translations for Privacy Guard.
    
    Due that on the one hand a lot user are not familiar
    with permissions and how to read them and on the
    other user want to have a faster easy access to
    setup privacy guard for several apps this small
    manager assist the user to setup.
    
    Privacy Guard Manager shows a list off all installed apps
    Per default it shows only user apps which have critical
    permissions privacy guard can protect.
    The user can optional show in the list as well all system apps
    or turn off the filter to show all apps undependent of the
    permission the app has. To activate privacy guard for an
    app a simple click on the entry activates it so that the user
    can go fast trough the list. Longpress opens app detail page.
    Initial help dialog tells the user how to use it.
    
    Due the filter user who are unfimiliar with permissions can
    fast see which apps are effected without deeper knowledge
    
    cheers
    
    SlimRoms
    
    PatchSet3:
    - go inline with AOSP style
    - move PrivacyGuard by default into manager
    - change behaviour off active icons
    
    PatchSet4:
    - change app icon size to 36dip
    
    PatchSet5:
    - string change
    
    PatchSet6:
    - add reset option to disable privacy guard for
      all shown apps in the list
    
    PatchSet7:
    - Cleaned up the intro dialog a bit.
    
    PatchSet8+9:
    - cleanup
    - fix alignment of top checkbox
    
    PatchSet10:
    - fix permission filter
    
    PatchSet11:
    - improve icon loading
    
    Change-Id: I6bc886393bf90f8daed92b66d5fee1ce9e1accd0
    
    Settings: privacy Guard enhance app logic
    
    This commit changes the following
    
    - do not show apps signed with the plaform certificate at all
      (like settings, systemUI, android system etc)
      IMO the user should trust the android signed apps to be save and this will
      prevent weird behaviours user can get when they activate it on this packages
      all other system apps (like calculator, browser) and all which are not signed with
      android key are not effected.
    
    - to go inline with AOSP disable the checkbox in detail app screen
      for built in apps
    
    cheers
    
    SlimRoms
    
    Change-Id: I12b5522f6e58047173778381fb7290bb1c71bab3
    
    Fix IllegalArgumentException in Privacy Guard Manager
    
    On tablets such as manta a this occurs.  To reproduce go into PGM, press
    the back button one time, then go back into PGM.
    
    http://pastebin.com/N7XAybUB
    
    Remove privacy guard fragment in onDestroyView
    
    Change-Id: I61cebe273fa6fd514f18c86894acd176701148f6
    
    Check if fragment transaction is empty before attempting to remove it
    
    Change-Id: I08591c89559d33fee30ac2934fdbcc30637147da
    
    PrivacyGuard fragment fix - change from empty check to isDestroyed check
    
    Change-Id: I52fe44b7446cd16793df6ac9cdbe31e2ddd70210
    Signed-off-by: Paul Beeler <sparksco@gmail.com>
    
    PrivacyGuardManager: Reduce calls to getFragmentManager
    
    Change-Id: I74f07a58b09cf0e9114d0c1c56e9a97d55ab0900

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -83,7 +83,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     // SMS Security
     private static final String KEY_SMS_SECURITY_CHECK_PREF = "sms_security_check_limit";
-    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
     private static final String KEY_APP_SECURITY_CATEGORY = "app_security";
 
     DevicePolicyManager mDPM;
@@ -108,7 +107,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
     // SMS Security
     private ListPreference mSmsSecurityCheck;
-    private CheckBoxPreference mPrivacyGuardDefault;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -270,28 +268,20 @@ public class SecuritySettings extends SettingsPreferenceFragment
             } else {
                 mToggleVerifyApps.setEnabled(false);
             }
-		}
-		// App security settings
-		addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
+	}
+	// App security settings
+	addPreferencesFromResource(R.xml.security_settings_app_cyanogenmod);
+	mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
+	if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
 		mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
-		if (pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-			mSmsSecurityCheck = (ListPreference) root.findPreference(KEY_SMS_SECURITY_CHECK_PREF);
-			mSmsSecurityCheck.setOnPreferenceChangeListener(this);
-			int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
-			updateSmsSecuritySummary(smsSecurityCheck);
-		} else {
-			PreferenceGroup appCategory = (PreferenceGroup)
-					root.findPreference(KEY_APP_SECURITY_CATEGORY);
-			appCategory.removePreference(mSmsSecurityCheck);
-		}
-
-		mPrivacyGuardDefault = (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
-		try {
-			mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
-					Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
-		} catch (SettingNotFoundException e) {
-			mPrivacyGuardDefault.setChecked(false);
-		}
+		mSmsSecurityCheck.setOnPreferenceChangeListener(this);
+		int smsSecurityCheck = Integer.valueOf(mSmsSecurityCheck.getValue());
+		updateSmsSecuritySummary(smsSecurityCheck);
+	} else {
+		PreferenceGroup appCategory = (PreferenceGroup)
+				root.findPreference(KEY_APP_SECURITY_CATEGORY);
+		appCategory.removePreference(mSmsSecurityCheck);
+	}
 
         return root;
     }
@@ -510,9 +500,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         } else if (KEY_TOGGLE_VERIFY_APPLICATIONS.equals(key)) {
             Settings.Global.putInt(getContentResolver(), Settings.Global.PACKAGE_VERIFIER_ENABLE,
                     mToggleVerifyApps.isChecked() ? 1 : 0);
-        } else if (KEY_PRIVACY_GUARD_DEFAULT.equals(key)) {
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.PRIVACY_GUARD_DEFAULT,
-                    mPrivacyGuardDefault.isChecked() ? 1 : 0);
         } else {
             // If we didn't handle it, let preferences handle it.
             return super.onPreferenceTreeClick(preferenceScreen, preference);
